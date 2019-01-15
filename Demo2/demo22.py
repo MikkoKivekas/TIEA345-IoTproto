50c72ec0e314a2a4aa41b31fc7b92bbb34154002
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-01-15 19:28:06 +0200

    Update demo22.py

--- Demo2/demo22.py
+++ Demo2/demo22.py
@@ -4,16 +4,26 @@ import time
 GPIO.setmode(GPIO.BCM)
 
 pir = 5
+nappi = 6
 
 GPIO.setup(pir, GPIO.IN)
+GPIO.setup(nappi, GPIO.IN)
 
 while 1:
     i=GPIO.input(pir)
     if i==0:
-        print 1
+        print 0
         time.sleep(1)
     elif i==1:
-        print 0
+        print 1
+        time.sleep(1)
+        
+    j=GPIO.input(nappi)
+    if j==0:
+        print "ei nappia"
+        time.sleep(1)
+    elif j==1:
+        print "nappi"
         time.sleep(1)
 
 GPIO.cleanup()
