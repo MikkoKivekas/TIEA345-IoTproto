0cd3faf2461d88d63f8fb3c33519bc340891e9d9
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-01-15 19:29:46 +0200

    Update demo22.py

--- Demo2/demo22.py
+++ Demo2/demo22.py
@@ -13,17 +13,17 @@ while 1:
     i=GPIO.input(pir)
     if i==0:
         print 0
-        time.sleep(1)
+        time.sleep(0.1)
     elif i==1:
         print 1
-        time.sleep(1)
+        time.sleep(0.1)
         
     j=GPIO.input(nappi)
     if j==0:
         print "ei nappia"
-        time.sleep(1)
+        time.sleep(0.1)
     elif j==1:
         print "nappi"
-        time.sleep(1)
+        time.sleep(0.1)
 
 GPIO.cleanup()
