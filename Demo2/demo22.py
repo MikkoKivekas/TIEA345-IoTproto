66e5aa6689bdf31b3259cec0ab81b223f8ebd80e
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-01-15 19:10:41 +0200

    Create demo22.py

---
+++ Demo2/demo22.py
@@ -0,0 +1,18 @@
+import RPi.GPIO as GPIO
+
+GPIO.setmode(GPIO.BCM)
+
+pir = 5
+
+GPIO.setup(pir, GPIO.IN)
+
+while TRUE
+    i=GPIO.input(pir)
+    if i==0:
+        print 1
+        time.sleep(0.1)
+    elif i==1:
+        print 0
+        time.sleep(0.1)
+
+GPIO.cleanup()
