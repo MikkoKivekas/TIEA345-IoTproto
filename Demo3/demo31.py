da570bba6e1ed83cd26920bd16aa0697dff19fb1
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-01-22 16:53:12 +0200

    Update demo31.py

--- Demo3/demo31.py
+++ Demo3/demo31.py
@@ -9,9 +9,12 @@ pin = 12
 #Senssorin luenta
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
+#lämpötila kelvineinä, koska fyysikon on pakko
+temperature = temperarture + 273
+
 #Tulsotus
 if humidity is not None and temperature is not None:
-    print('Temperature={0:0.1f} °C  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('Temperature={0:0.1f} K  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
     
