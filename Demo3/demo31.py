2e8a86cc5197bceeef5347a3015bd949b4c78a7f
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-01-22 16:51:55 +0200

    Update demo31.py

--- Demo3/demo31.py
+++ Demo3/demo31.py
@@ -11,7 +11,7 @@ humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
 #Tulsotus
 if humidity is not None and temperature is not None:
-    print('Temperature={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+    print('Temperature={0:0.1f} °C  Humidity={1:0.1f}%'.format(temperature, humidity))
 else:
     print('Failed to get reading. Try again!')
     
