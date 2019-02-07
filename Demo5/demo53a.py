aa14ddc2ac345eb8580264c90d6f4105ca77a857
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-02-07 21:01:23 +0200

    Update demo53a.py

--- Demo5/demo53a.py
+++ Demo5/demo53a.py
@@ -3,9 +3,9 @@ import random
 
 red = redis.StrictRedis(host='localhost', port=6379, db=0)
 # avaimen asettaminen  
-    #red.set('key','value')
+    red.set('key','value')
 # avaimen hakeminen  
-    #red.get('key')
+    red.get('key')
 # publish  
     #red.publish("topic","value")
     
