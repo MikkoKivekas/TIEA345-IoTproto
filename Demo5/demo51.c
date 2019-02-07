4ae91f2d0aa58b385bbb24638139a2040303c088
MikkoKivekas <46553623+MikkoKivekas@users.noreply.github.com>
2019-02-07 17:38:09 +0200

    Update demo51.c

--- Demo5/demo51.c
+++ Demo5/demo51.c
@@ -1,1 +1,24 @@
-kek
+#include <wiringPi.h>
+int main()
+{
+
+  wiringPiSetupGpio();
+
+  pinMode(14, INPUT); //pir
+  pinMode(6, INPUT);  //nappi
+  pinMode(5, OUTPUT);//led
+  digitalWrite(5, LOW);
+    
+    // while loop terminates when button is pressed
+    while (digitalRead(6)==0) 
+  {
+    if(digitalRead(5)==1){
+      digitalWrite(5, HIGH);
+      printf("Pin 6 tila on HIGH\n");
+    }
+      digitalWrite(5, LOW);
+      Sleep(1000);//waits for a second
+  }
+
+    return 0;
+}
